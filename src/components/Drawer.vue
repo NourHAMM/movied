443577aa285d8beab342d207b71297a07044e68b
NourHAMM <60269159+NourHAMM@users.noreply.github.com>
2020-03-27 22:50:07 +0400

    Drawer logic

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,13 +1,19 @@
 <template>
-   <v-navigation-drawer app>
+   <v-navigation-drawer :value="drawer" app clipped>
  
   </v-navigation-drawer>
 
 </template>
 
 <script>
+import { mapGetters } from "vuex"
 export default {
     name: "Drawer",
+    computed: {
+        ...mapGetters({
+            drawer: "getDrawer"
+    }),
+    }
 
 }
 </script>
